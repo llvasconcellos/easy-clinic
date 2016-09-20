dd1fc0d69041ea43bdbc5c9e4b0b5b634a4ee00f
Leonardo Lima de Vasconcellos <leo.lima.web@gmail.com>
2016-09-20 18:12:08 -0300

    app skeleton

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -1,4 +1,4 @@
-Router.configure({
+/*Router.configure({
   layoutTemplate: 'MasterLayout',
   loadingTemplate: 'Loading',
   notFoundTemplate: 'NotFound'
@@ -9,4 +9,23 @@ Router.route('/', {
   name: 'home',
   controller: 'HomeController',
   where: 'client'
+});*/
+
+
+FlowRouter.route('/', {
+    action: function() {
+        FlowRouter.go('/home');
+    }
+});
+
+FlowRouter.route('/home', {
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "home"});
+    }
+});
+
+FlowRouter.route('/pagina2', {
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "pagina2"});
+    }
 });
