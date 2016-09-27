8f1a4ddfa9b11a7feaf931854e197326ac82ea2b
Leonardo Lima de Vasconcellos <leo.lima.web@gmail.com>
2016-09-26 21:30:45 -0300

    Added Gravatar. IssueID #29 @0.5h

--- app/client/views/common/navigation.js
+++ app/client/views/common/navigation.js
@@ -1,6 +1,13 @@
 Template.navigation.rendered = function(){
+	// Initialize metisMenu
+	$('#side-menu').metisMenu();
 
-    // Initialize metisMenu
-    $('#side-menu').metisMenu();
-
+	if(Meteor.user()) {
+		var email = Meteor.user().emails[0].address;
+		var url = Gravatar.imageUrl(email, {
+			size: 50,
+			default: 'images/default-user-image.png'
+		});
+		$('#mini-profile-img').attr('src', url);
+	}
 };
