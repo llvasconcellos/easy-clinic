9d900f2ffbc2e90277cf887db82f9da9577a8127
Leonardo Lima de Vasconcellos <leo.lima.web@gmail.com>
2016-10-10 12:01:07 -0300

    fixes #64 @15m
    Former-commit-id: f39e7b807565c0b9e19f65172afa612932eae2c8

---
+++ app/lib/i18n/es.js
@@ -0,0 +1,18 @@
+T9n.map('pt', {
+	appName: 'Clínica Fácil',
+	firstName: 'Nombres',
+	lastName: 'Apellidos',
+	group: "Papel",
+	groupMD: "Médico(a)",
+	groupNurse: "Enfermero(a)",
+	groupReception: "Recepcionista",
+	groupAdmin: "Administración",
+	name: "Nombre",
+	enabled: "Activo",
+	disabled: "Inactivo",
+	edit: "Editar",
+	verifyEmailSubject: "[@{appName}] Verifique su dirección de e-mail",
+	verifyEmailBody: "Hola, @{name}.\n\nPara confirmar su dirección de e-mail (@{emailAddress}) visite la siguiente liga:\n\n@{url}\n\n Si usted no solicitó esa verificación, por favor ignore ese email. Cualquier duda entre en contacto con nosotros: @{supportEmail}.",
+	resetPasswordEmailSubject: "Cómo cambiar su contraseña en el @{appName}",
+	resetPasswordEmailBody: "Hola, @{name}.\n\nPara cambiar su contraseña visite la siguiente liga:\n\n@{url}\n\n Gracias!"
+});
